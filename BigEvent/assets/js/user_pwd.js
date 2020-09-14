07d9bbf5f12e18d00efbfd5a1a601c062b31952a
Mac <>
2020-09-14 14:12:21 +0800

    user_info

--- BigEvent/assets/js/user_pwd.js
+++ BigEvent/assets/js/user_pwd.js
@@ -1,4 +1,21 @@
 $(function () {
+  var form = layui.form;
+  //判断新密码和确认密码是否一致
+
+  form.verify({
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    samePwd: function (value) {
+      if (value === $('[name=oldPwd]').val()) {
+        return '新旧密码不能相同！'
+      }
+    },
+    rePwd: function (value) {
+      if (value !== $('[name=newPwd]').val()) {
+        return '两次密码不一致！'
+      }
+    }
+  })
+
   // 监听表单的提交事件
   $('.layui-form').on('submit', function (e) {
     console.log($(this).serialize());
